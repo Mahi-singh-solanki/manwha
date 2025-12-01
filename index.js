af8a3598cae4565f56227a86f43471355105905d
Mahi <mahipalsinghapsit0@gmail.com>
2025-12-01 22:50:40 +0530

    done with changes

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PROXY_PASS = process.env.WS_PROXY_PASS;
 const app = express();
 app.use(body_parser.json());
 const corsOptions = {
-  origin:"https://mahi-manwha.netlify.app", // Only allow requests from your frontend URL
+  origin:"https://mahi-manwha.netlify.app",
+  origin:"http://localhost:5173" // Only allow requests from your frontend URL
 };
 app.use(cors(corsOptions)); 
 
